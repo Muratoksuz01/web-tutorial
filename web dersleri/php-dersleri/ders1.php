3fa83e01753f998eef6a600c43e36a4f8f691e34
Muratoksuz01 <muratoksuz208@gmail.com>
2024-04-26 01:24:45 +0300

    ı had learned before

---
+++ web dersleri/php-dersleri/ders1.php
@@ -0,0 +1,61 @@
+<?php
+/*
+$veri="ali";
+echo gettype($veri);//tipini veriyor
+*/
+
+/*
+define("degisken","değer");//değişmez bir daha
+echo degisken;//diyerek cağırabilirsin
+*/
+
+
+// $a=10;
+// $b=12;
+// echo $a>$b ? "a buyuktur":"b buyuktur";//kısa if else ulllanımı
+
+
+/*
+$meta= array();
+$meta ['title'] = 'şahin Ersever - Udemy Dersleri';
+$meta ['desc'] = 'Php ile sıfırdan ileri seviyeye Udemy Dersleri';// bunu yazdırmak için farklı bir yazdırma sekli kullandık 
+///ddikkate et 
+$meta ['keyw'] = 'php, pdo, php oop, php mvc';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $meta['title']?></title>
+</head>
+<body>
+    <h1><?=$meta['desc']?></h1>
+</body>
+</html>
+*/
+
+
+
+// $urun=["monitor","klavye","usb"];/* string dizilerini yazdırma */
+// foreach($urun as $deger)
+//     echo $deger."<br>";
+
+
+// $urundetay = [
+//     'urun-adi' => 'monitör',
+//     'marka'=>'samsung',
+//     'fiyat' => '5000TL'
+//     ];
+// foreach ($urundetay as $anahrar => $değer) {/* bu tür anahtar değer ilişkisi varsa */
+//     echo $anahrar."---->".$deger."<br>";
+// }
+
+
+
+// function isim(){/*foksiyonlarda böyle  */ 
+//     echo "adaa";
+// }
+// isim();
+
